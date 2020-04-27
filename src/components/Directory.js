69ce703a090693bedb85a4df4228de73bfe5ddd8
Paul Harwood <paul@harwood-leon.copm>
2020-04-28 00:13:06 +0100

    see details button

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -47,7 +47,7 @@ class BlogRoll extends React.Component {
                   <br />
                   <br />
                   <Link className="button" to={post.fields.slug}>
-                    Keep Reading →
+                    See details
                   </Link>
                 </p>
               </article>
